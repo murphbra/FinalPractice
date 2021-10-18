f7a24bb993e420e62ff5b52c914c6ac1d55a6f4d
murphbra <murphbra@oregonstate.edu>
2021-10-18 13:53:03 -0600

    update

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ router.get('/boats/:id', function (req, res) {
                 res.status(404).json({ 'Error': 'No boat exists with this boat_id exists' });
             } else {
                 // Return the 0th element which is the boat with this id
-                res.status(200).json(lodging[0]);
+                res.status(200).json(boat[0]);
             }
         });
 });
