4c112abfdd7c2a2a208a7ebbd24fca7c3a566d6c
murphbra <murphbra@oregonstate.edu>
2021-10-17 22:42:39 -0600

    update

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ router.post('/boats', function (req, res) {
     } else {
         post_boat(req.body.name, req.body.type, req.body.length)
         .then(new_boat => { 
-            new_boat.self = "https://cs493a3.wm.r.appspot.com/boats/" + new_boat.id; 
+            new_boat[self] = "https://cs493a3.wm.r.appspot.com/boats/" + string(new_boat.id); 
             res.status(201).send(new_boat); 
         }); 
             //res.status(201).send('{ "name": '+ '"' + new_boat.name + '"' + ',' + '\n' + '"type": ' + '"' + new_boat.type +'"' + ',' + '\n' + '"length": ' + new_boat.length  + ' }') });
