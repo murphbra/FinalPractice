ba119a15e90149b6b0cc6905968c1516598da453
murphbra <murphbra@oregonstate.edu>
2021-11-01 01:46:39 -0700

    update

--- server.js
+++ server.js
@@ -255,7 +255,8 @@ router.put('/boats/:id', function (req, res) {
                         boat[0].type = req.body.type;
                         boat[0].length = req.body.length;
                         boat[0].self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + boat[0].id; 
-                        res.status(200).json(boat[0]).end();
+                        res.location(boat[0].self); 
+                        res.status(303).json(boat[0]).end();
 
                     }
                 })
