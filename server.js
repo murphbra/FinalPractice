58bb9a1916903209ed8f6a7065f11db03bb19b87
murphbra <murphbra@oregonstate.edu>
2021-10-31 22:27:34 -0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Author: Brayden Murphy
-//CS 493 Assignment 3
+//CS 493 Assignment 5
 // Adapted from example code provided in course materials for CS 493 
 
 const express = require('express');
@@ -11,8 +11,6 @@ const bodyParser = require('body-parser');
 const datastore = new Datastore();
 
 const BOAT = "Boat"; 
-
-const SLIP = "Slip"; 
 
 const router = express.Router();
 
@@ -33,14 +31,6 @@ function post_boat(name, type, length) {
         new_boat.id = key.id; 
         return new_boat });
 }
-
-function post_slip(number) {
-    var key = datastore.key(SLIP);
-    const new_slip = { "number": number, "current_boat": null };
-    return datastore.save({ "key": key, "data": new_slip }).then(() => { 
-        new_slip.id = key.id; 
-        return new_slip });
-}
 /**
  * The function datastore.query returns an array, where the element at index 0
  * is itself an array. Each element in the array at element 0 is a JSON object
@@ -49,16 +39,6 @@ function post_slip(number) {
 
 function get_boats() {
     const q = datastore.createQuery(BOAT);
-    return datastore.runQuery(q).then((entities) => {
-        // Use Array.map to call the function fromDatastore. This function
-        // adds id attribute to every element in the array at element 0 of
-        // the variable entities
-        return entities[0].map(fromDatastore);
-    });
-}
-
-function get_slips() {
-    const q = datastore.createQuery(SLIP);
     return datastore.runQuery(q).then((entities) => {
         // Use Array.map to call the function fromDatastore. This function
         // adds id attribute to every element in the array at element 0 of
@@ -81,41 +61,10 @@ function get_boat(id) {
     });
 }
 
-function get_slip(id) {
-    const key = datastore.key([SLIP, parseInt(id, 10)]);
-    return datastore.get(key).then((entity) => {
-        if (entity[0] === undefined || entity[0] === null) {
-            // No entity found. Don't try to add the id attribute
-            return entity;
-        } else {
-            // Use Array.map to call the function fromDatastore. This function
-            // adds id attribute to every element in the array entity
-            return entity.map(fromDatastore);
-        }
-    });
-}
-
 function patch_boat(id, name, type, length) {
     const key = datastore.key([BOAT, parseInt(id, 10)]);
     const boat = { "name": name, "type": type, "length": length };
     return datastore.save({ "key": key, "data": boat });
-}
-
-function put_boat_in_slip(slip_id, boat_id, number) {
-    const key = datastore.key([SLIP, parseInt(slip_id, 10)]);
-    const slip = { "number": number, "current_boat": boat_id}; 
-    return datastore.save({ "key": key, "data": slip });
-    }
-
-function boat_departs_slip(slip_id, number) {
-    const key = datastore.key([SLIP, parseInt(slip_id, 10)]);
-    const slip = { "number": number, "current_boat": null}; 
-    return datastore.save({ "key": key, "data": slip });
-    }
-
-function delete_slip(id) {
-    const key = datastore.key([SLIP, parseInt(id, 10)]);
-    return datastore.delete(key); 
 }
 
 function delete_boat(id) {
@@ -132,93 +81,67 @@ router.get('/boats', function (req, res) {
 
             for(var i = 0; i< boats.length ; i++)
             {
-                boats[i].self = "https://cs493a3.wm.r.appspot.com/boats/" + boats[i].id; 
+                boats[i].self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + boats[i].id; 
             }
             res.status(200).json(boats);
         });
 });
 
-router.get('/slips', function (req, res) {
-    const slips = get_slips().then((slips) => {
-
-            for(var i = 0; i< slips.length ; i++)
-            {
-                slips[i].self = "https://cs493a3.wm.r.appspot.com/slips/" + slips[i].id; 
-            }
-            res.status(200).json(slips);
-        });
-});
-
 router.post('/boats', function (req, res) {
+    if(req.get('content-type') !== 'application/json'){
+        res.status(415).json({'Error': 'Server only accepts application/json data.'}).end(); 
+    }
+    if(req.accepts !== 'application/json')
+    {
+        res.status(406).json({'Error': 'Client must accept application/json'}).end(); 
+    }
+
     if(req.body.length === undefined)
     {
         res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
     } 
-    if(req.body.name === undefined)
+    else if(req.body.name === undefined)
     {
         res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
     }
-    if(req.body.type === undefined)
+    else if(req.body.type === undefined)
     {
         res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
     }
     else 
     {
-        post_boat(req.body.name, req.body.type, req.body.length).then(new_boat => { 
-            new_boat.self = "https://cs493a3.wm.r.appspot.com/boats/" + new_boat.id; 
-            res.status(201).send(new_boat); 
-        }); 
-    }
-});
-
-router.post('/slips', function (req, res) {
-    if(req.body.number === undefined)
-    {
-        res.status(400).json({ 'Error': 'The request object is missing the required number' }).end(); 
-    } 
-    else 
-    {
-        post_slip(req.body.number).then(new_slip => { 
-            new_slip.self = "https://cs493a3.wm.r.appspot.com/slips/" + new_slip.id; 
-            res.status(201).send(new_slip); 
-        }); 
-    }
-});
-
-router.put('/slips/:slip_id/:boat_id', function (req, res) {
-    get_boat(req.params.boat_id)
-    .then(boat => 
+        const attributes = Object.keys(req.body);
+        if(attributes.length > 3)
         {
-            if (boat[0] === undefined || boat[0] === null) 
+            res.status(400).json({'Error': 'The request included at least one non-supported attribute'}).end(); 
+        }
+        if(req.body.name.length > 20)
+        {
+            res.status(400).json({'Error': 'Boat name attribute must be 20 characters or less'}).end(); 
+        }
+        var alphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
+        for(x=0;x<req.body.name.length;x++)
+        {
+            if(!alphaNum.includes(req.body.name[x]))
             {
-                res.status(404).json({ 'Error': 'The specified boat and/or slip does not exist' }).end(); 
+                res.status(400).json({'Error': 'Boat name characters must be alphanumeric'}).end(); 
             }
-
-            else
+        }
+        get_boats().then((boats) => {
+            for(i=0;i<boats.length;i++)
             {
-                get_slip(req.params.slip_id)
-                .then (slip =>
-                    {
-                        if (slip[0] === undefined || slip[0] === null) 
-                        {
-                            res.status(404).json({ 'Error': 'The specified boat and/or slip does not exist' }).end(); 
-                        }
-            
-                        else if (slip[0].current_boat !== null)
-                        {
-                            res.status(403).json({ 'Error': 'The slip is not empty'}).end(); 
-                        }
-
-                        else
-                        {
-                            var slipNumber = slip[0].number; 
-                            put_boat_in_slip(req.params.slip_id, req.params.boat_id, slipNumber); 
-                            res.status(204).end(); 
-                        }
-                    })
+                if(boats[i].name == req.body.name)
+                {
+                    res.status(403).json({'Error': 'A boat with that name already exits'}).end(); 
                 }
+            }
         })
-}); 
+        post_boat(req.body.name, req.body.type, req.body.length).then(new_boat => { 
+            new_boat.self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + new_boat.id; 
+            res.status(201).json(new_boat); 
+        }); 
+    }
+});
 
 
 router.get('/boats/:id', function (req, res) {
@@ -227,20 +150,8 @@ router.get('/boats/:id', function (req, res) {
             if (boat[0] === undefined || boat[0] === null) {
                 res.status(404).json({ 'Error': 'No boat with this boat_id exists' });
             } else {
-                boat[0].self = "https://cs493a3.wm.r.appspot.com/boats/" + boat[0].id; 
+                boat[0].self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + boat[0].id; 
                 res.status(200).json(boat[0]);
-            }
-        });
-});
-
-router.get('/slips/:id', function (req, res) {
-    get_slip(req.params.id)
-        .then(slip => {
-            if (slip[0] === undefined || slip[0] === null) {
-                res.status(404).json({ 'Error': 'No slip with this slip_id exists' });
-            } else {
-                slip[0].self = "https://cs493a3.wm.r.appspot.com/slips/" + slip[0].id; 
-                res.status(200).json(slip[0]);
             }
         });
 });
@@ -268,65 +179,13 @@ router.patch('/boats/:id', function (req, res) {
                 boat[0].name = req.body.name;
                 boat[0].type = req.body.type;
                 boat[0].length = req.body.length;
-                boat[0].self = "https://cs493a3.wm.r.appspot.com/boats/" + boat[0].id; 
+                boat[0].self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + boat[0].id; 
                 res.status(200).json(boat[0]);
             }
         });
 
     }
 });
-
-router.delete('/slips/:slip_id/:boat_id', function (req, res) {
-    get_boat(req.params.boat_id)
-    .then(boat => 
-        {
-            if (boat[0] === undefined || boat[0] === null) 
-            {
-                res.status(404).json({ 'Error': 'No boat with this boat_id is at the slip with this slip_id' }).end(); 
-            }
-
-            else
-            {
-                get_slip(req.params.slip_id)
-                .then (slip =>
-                    {
-                        if (slip[0] === undefined || slip[0] === null) 
-                        {
-                            res.status(404).json({ 'Error': 'No boat with this boat_id is at the slip with this slip_id' }).end(); 
-                        }
-            
-                        else if (slip[0].current_boat != boat[0].id)
-                        {
-                            res.status(404).json({ 'Error': 'No boat with this boat_id is at the slip with this slip_id'}).end(); 
-                        }
-
-                        else
-                        {
-                            var slipNumber = slip[0].number; 
-                            boat_departs_slip(req.params.slip_id, slipNumber); 
-                            res.status(204).end(); 
-                        }
-                    })
-                }
-        })
-}); 
-
-router.delete('/slips/:slip_id', function(req, res) {
-    get_slip(req.params.slip_id)
-    .then (slip =>
-        {
-            if (slip[0] === undefined || slip[0] === null) 
-            {
-                // The 0th element is undefined. This means there is no lodging with this id
-                res.status(404).json({ 'Error': 'No slip with this slip_id exists' }).end(); 
-            }
-            else
-            {
-                delete_slip(req.params.slip_id).then(res.status(204).end()); 
-            }
-        })
-
-}); 
 
 router.delete('/boats/:boat_id', function(req, res) {
     get_boat(req.params.boat_id)
@@ -339,21 +198,6 @@ router.delete('/boats/:boat_id', function(req, res) {
             }
             else
             {
-                const slips = get_slips().then((slips) => { 
-                    for(var i = 0; i < slips.length; i++)
-                    {
-                        if(slips[i].current_boat !== null)
-                        {
-                            if(slips[i].current_boat == req.params.boat_id)
-                            {
-                                var slipsID = slips[i].id; 
-                                var slipsNum = slips[i].number; 
-                                boat_departs_slip(slipsID, slipsNum); 
-                            }
-                        }
-                    }
-                }); 
-
                 delete_boat(req.params.boat_id).then(res.status(204).end()); 
             }
         })
