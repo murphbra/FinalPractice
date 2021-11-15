c0c75593061072aba7699ee549326db1afc57503
murphbra <murphbra@oregonstate.edu>
2021-11-14 17:07:14 -0800

    update

--- server.js
+++ server.js
@@ -1,419 +1,158 @@
 //Author: Brayden Murphy
-//CS 493 Assignment 5
+//CS 493 Assignment 7
 // Adapted from example code provided in course materials for CS 493 
 
-const json2html = require('json-to-html');
 const express = require('express');
 const app = express();
 
-const { Datastore } = require('@google-cloud/datastore');
+const json2html = require('json-to-html');
+
+const {Datastore} = require('@google-cloud/datastore');
+
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const datastore = new Datastore();
 
-const BOAT = "Boat"; 
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+const LODGING = "Lodging";
 
 const router = express.Router();
+const login = express.Router();
+
+const CLIENT_ID = 'YOUR_CLIENT_ID';
+const CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
+const DOMAIN = 'YOUR_DOMAIN.auth0.com';
 
 app.use(bodyParser.json());
 
-function fromDatastore(item) {
+function fromDatastore(item){
     item.id = item[Datastore.KEY].id;
     return item;
 }
 
-app.set('trust proxy', true); 
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://${DOMAIN}/.well-known/jwks.json`
+    }),
+  
+    // Validate the audience and the issuer.
+    issuer: `https://${DOMAIN}/`,
+    algorithms: ['RS256']
+  });
+
 /* ------------- Begin Lodging Model Functions ------------- */
-
-function post_boat(name, type, length) {
-    var key = datastore.key(BOAT);
-    const new_boat = { "name": name, "type": type, "length": length };
-    return datastore.save({ "key": key, "data": new_boat }).then(() => { 
-        new_boat.id = key.id; 
-        return new_boat });
-}
-/**
- * The function datastore.query returns an array, where the element at index 0
- * is itself an array. Each element in the array at element 0 is a JSON object
- * with an entity fromt the type "Lodging".
- */
-
-function get_boats() {
-    const q = datastore.createQuery(BOAT);
-    return datastore.runQuery(q).then((entities) => {
-        // Use Array.map to call the function fromDatastore. This function
-        // adds id attribute to every element in the array at element 0 of
-        // the variable entities
-        return entities[0].map(fromDatastore);
-    });
+function post_lodging(name, description, price, owner){
+    var key = datastore.key(LODGING);
+	const new_lodging = {"name": name, "description": description, "price": price, "owner":owner};
+	return datastore.save({"key":key, "data":new_lodging}).then(() => {return key});
 }
 
-function get_boat(id) {
-    const key = datastore.key([BOAT, parseInt(id, 10)]);
-    return datastore.get(key).then((entity) => {
-        if (entity[0] === undefined || entity[0] === null) {
-            // No entity found. Don't try to add the id attribute
-            return entity;
-        } else {
-            // Use Array.map to call the function fromDatastore. This function
-            // adds id attribute to every element in the array entity
-            return entity.map(fromDatastore);
+function get_lodgings(owner){
+	const q = datastore.createQuery(LODGING);
+	return datastore.runQuery(q).then( (entities) => {
+			return entities[0].map(fromDatastore).filter( item => item.owner === owner );
+		});
+}
+
+function get_lodgings_unprotected(){
+	const q = datastore.createQuery(LODGING);
+	return datastore.runQuery(q).then( (entities) => {
+			return entities[0].map(fromDatastore);
+		});
+}
+
+function get_lodging(id, owner){
+    const key = datastore.key([LODGING, parseInt(id,10)]);
+    return datastore.get(key).then( (data) => {
+            return fromDatastore(data[0]);
         }
-    });
+    );
 }
-
-function put_boat(id, name, type, length) {
-    const key = datastore.key([BOAT, parseInt(id, 10)]);
-    const boat = { "name": name, "type": type, "length": length };
-    return datastore.save({ "key": key, "data": boat });
-}
-
-function delete_boat(id) {
-    const key = datastore.key([BOAT, parseInt(id, 10)]);
-    return datastore.delete(key); 
-}
-
 
 /* ------------- End Model Functions ------------- */
 
 /* ------------- Begin Controller Functions ------------- */
 
-router.get('/boats', function (req, res) {
-    const boats = get_boats().then((boats) => {
-
-            for(var i = 0; i< boats.length ; i++)
-            {
-                boats[i].self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + boats[i].id; 
-            }
-            res.status(200).json(boats);
-        });
+router.get('/', checkJwt, function(req, res){
+    console.log('jwt' + req.user);
+    console.log(JSON.stringify(req.user));
+    const lodgings = get_lodgings(req.user.name)
+	.then( (lodgings) => {
+        res.status(200).json(lodgings);
+    });
 });
 
-router.post('/boats', function (req, res) {
+router.get('/unsecure', function(req, res){
+    const lodgings = get_lodgings_unprotected()
+	.then( (lodgings) => {
+        res.status(200).json(lodgings);
+    });
+});
+
+router.get('/:id', checkJwt, function(req, res){
+        console.log('jwt' + req.user);
+    const lodgings = get_lodging(req.params.id)
+	.then( (lodging) => {
+        const accepts = req.accepts(['application/json', 'text/html']);
+        if(lodging.owner && lodging.owner !== req.user.name){
+            res.status(403).send('Forbidden');
+        } else if(!accepts){
+            res.status(406).send('Not Acceptable');
+        } else if(accepts === 'application/json'){
+            res.status(200).json(lodging);
+        } else if(accepts === 'text/html'){
+            res.status(200).send(json2html(lodging).slice(1,-1));
+        } else { res.status(500).send('Content type got messed up!'); }
+    });
+});
+
+router.post('/', checkJwt, function(req, res){
     if(req.get('content-type') !== 'application/json'){
-        res.status(415).json({'Error': 'Server only accepts application/json data.'}).end(); 
-        return; 
+        res.status(415).send('Server only accepts application/json data.')
     }
-    const accepts = req.accepts(['application/json']); 
-    if(!accepts)
-    {
-        res.status(406).json({'Error': 'Client must accept application/json'}).end(); 
-        return; 
-    }
-
-    if(req.body.length === undefined)
-    {
-        res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
-        return; 
-    } 
-    else if(req.body.name === undefined)
-    {
-        res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
-        return; 
-    }
-    else if(req.body.type === undefined)
-    {
-        res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
-        return; 
-    }
-    else 
-    {
-        const attributes = Object.keys(req.body);
-        if(attributes.length > 3)
-        {
-            res.status(400).json({'Error': 'The request included at least one non-supported attribute'}).end(); 
-            return; 
-        }
-        if(req.body.name.length > 20)
-        {
-            res.status(400).json({'Error': 'Boat name attribute must be 20 characters or less'}).end(); 
-            return; 
-        }
-        var alphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "; 
-        for(var x=0; x<req.body.name.length; x++)
-        {
-            if(!alphaNum.includes(req.body.name[x]))
-            {
-                res.status(400).json({'Error': 'Boat name characters must be alphanumeric'}).end();
-                return;  
-            }
-        }
-        get_boats().then((boats) => {
-            var repeat = false; 
-            for(var i=0; i<boats.length; i++)
-            {
-                if(boats[i].name == req.body.name)
-                {
-                    res.status(403).json({'Error': 'A boat with that name already exists'}).end(); 
-                    repeat = true; 
-                }
-            }
-            return repeat; 
-        }).then( (repeat) => { 
-            if(!repeat)
-            {
-                post_boat(req.body.name, req.body.type, req.body.length).then(new_boat => { 
-                    new_boat.self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + new_boat.id; 
-                    res.status(201).json(new_boat); 
-                    return; 
-                }); 
-            }
-        }); 
-    }
+    post_lodging(req.body.name, req.body.description, req.body.price, req.user.name)
+    .then( key => {
+        res.location(req.protocol + "://" + req.get('host') + req.baseUrl + '/' + key.id);
+        res.status(201).send('{ "id": ' + key.id + ' }')
+    } );
 });
 
+login.post('/', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    var options = { method: 'POST',
+            url: `https://${DOMAIN}/oauth/token`,
+            headers: { 'content-type': 'application/json' },
+            body:
+             { grant_type: 'password',
+               username: username,
+               password: password,
+               client_id: CLIENT_ID,
+               client_secret: CLIENT_SECRET },
+            json: true };
+    request(options, (error, response, body) => {
+        if (error){
+            res.status(500).send(error);
+        } else {
+            res.send(body);
+        }
+    });
 
-router.get('/boats/:id', function (req, res) {
-    get_boat(req.params.id)
-        .then(boat => {
-            if (boat[0] === undefined || boat[0] === null) {
-                res.status(404).json({ 'Error': 'No boat with this boat_id exists' });
-            } else {
-                boat[0].self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + boat[0].id; 
-                const accepts = req.accepts(['application/json', 'text/html']);
-                if(!accepts)
-                {
-                    res.status(406).json({'Error': 'Not Acceptable'});
-                } 
-                else if(accepts === 'application/json')
-                {
-                    res.status(200).json(boat[0]);
-                } 
-                else if(accepts === 'text/html')
-                {
-                    res.status(200).send(json2html(boat[0]).slice(1,-1));
-                } 
-            }
-        });
 });
-
-router.put('/boats/:id', function (req, res) {
-    if(req.get('content-type') !== 'application/json'){
-        res.status(415).json({'Error': 'Server only accepts application/json data.'}).end(); 
-        return; 
-    }
-    const accepts = req.accepts(['application/json']); 
-    if(!accepts)
-    {
-        res.status(406).json({'Error': 'Client must accept application/json'}).end(); 
-        return; 
-    }
-
-    if(req.body.length === undefined)
-    {
-        res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
-    } 
-    if(req.body.name === undefined)
-    {
-        res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
-    }
-    if(req.body.type === undefined)
-    {
-        res.status(400).json({ 'Error': 'The request object is missing at least one of the required attributes' }).end(); 
-    }
-    else{
-        const attributes = Object.keys(req.body);
-        if(attributes.length > 3)
-        {
-            res.status(400).json({'Error': 'The request included at least one non-supported attribute'}).end(); 
-            return; 
-        }
-        if(req.body.name.length > 20)
-        {
-            res.status(400).json({'Error': 'Boat name attribute must be 20 characters or less'}).end(); 
-            return; 
-        }
-        var alphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "; 
-        for(var x=0; x<req.body.name.length; x++)
-        {
-            if(!alphaNum.includes(req.body.name[x]))
-            {
-                res.status(400).json({'Error': 'Boat name characters must be alphanumeric'}).end();
-                return;  
-            }
-        }
-        get_boat(req.params.id)
-        .then(boat => {
-            if (boat[0] === undefined || boat[0] === null) {
-                res.status(404).json({ 'Error': 'No boat with this boat_id exists' });
-            } else {
-                get_boats().then((boats) => {
-                    var repeat = false;
-                    for(var i=0; i<boats.length; i++)
-                    {
-                        if(boats[i].name == req.body.name && boats[i].id != req.params.id)
-                        {
-                            res.status(403).json({'Error': 'A boat with that name already exists'}).end();
-                            repeat = true; 
-                        }
-                    }
-                    return repeat; 
-                }).then( (repeat) => {
-                    if(!repeat)
-                    {
-                        put_boat(req.params.id, req.body.name, req.body.type, req.body.length); 
-                        boat[0].name = req.body.name;
-                        boat[0].type = req.body.type;
-                        boat[0].length = req.body.length;
-                        boat[0].self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + boat[0].id; 
-                        res.location(boat[0].self); 
-                        res.status(303).json(boat[0]).end();
-                    }
-                })
-            }
-        });
-    }
-});
-
-
-router.patch('/boats/:id', function (req, res) { 
-    if(req.get('content-type') !== 'application/json')
-    {
-        res.status(415).json({'Error': 'Server only accepts application/json data.'}).end();
-        return; 
-    }
-    const accepts = req.accepts(['application/json']); 
-    if(!accepts)
-    {
-        res.status(406).json({'Error': 'Client must accept application/json'}).end(); 
-        return; 
-    }
-    const accepted = ["name", "type", "length"]; 
-    const attributes = Object.keys(req.body); 
-    var results = {}; 
-    results.body = attributes; 
-    for(var y = 0; y< attributes.length; y++)
-    {
-        if(!accepted.includes(attributes[y]))
-        {
-            res.status(400).json({'Error': "The request included at least one non-supported attribute"}).end();
-            return; 
-        }
-    }
-    if(attributes.includes("name"))
-    {
-        var alphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "; 
-        for(var x=0; x<req.body.name.length; x++)
-        {
-            if(!alphaNum.includes(req.body.name[x]))
-            {
-                res.status(400).json({'Error': 'Boat name characters must be alphanumeric'}).end();
-                return; 
-            }
-        }
-        if(req.body.name.length > 20)
-        {
-            res.status(400).json({'Error': 'Boat name attribute must be 20 characters or less'}).end(); 
-            return; 
-        }
-    }
-    get_boat(req.params.id)
-    .then(boat => {
-        if(boat[0] === undefined || boat[0] === null)
-        {
-            res.status(404).json({'Error': 'No boat with this boat_id exists'});
-        }
-        else 
-        {
-            get_boats().then((boats) => {
-                var duplicate = false; 
-                //for(var i=0; i<boats.length; i++)
-                if(attributes.includes("name"))
-                {
-                    for(var i=0; i<boats.length; i++)
-                    {
-                        if(boats[i].name == req.body.name && boats[i].id != req.params.id)
-                        {
-                            duplicate = true; 
-                            res.status(403).json({'Error': 'A boat with that name already exists'}).end();
-                        }  
-                    }
-                }
-                return duplicate; 
-            }).then ( (duplicate) => {
-                if(!duplicate)
-                {
-                    if(attributes.includes("name")) 
-                    {
-                        var name = req.body.name; 
-                    } 
-                    if(!attributes.includes("name")) 
-                    {
-                        var name = boat[0].name;
-                    } 
-                    
-                    if(attributes.includes("type")) 
-                    {
-                        var type = req.body.type; 
-                    } 
-                    if(!attributes.includes("type")) 
-                    {
-                        var type = boat[0].type; 
-                    } 
-                    
-                    if(attributes.includes("length")) 
-                    {
-                        var length = req.body.length; 
-                    } 
-                    if(!attributes.includes("length")) 
-                    {
-                        var length = boat[0].length; 
-                    } 
-
-                    put_boat(req.params.id, name, type, length); 
-                    boat[0].name = name; 
-                    boat[0].type = type; 
-                    boat[0].length = length; 
-                    boat[0].self = "https://cs493a5-330723.wm.r.appspot.com/boats/" + boat[0].id;
-                    res.status(200).json(boat[0]).end(); 
-                }
-            }); 
-        }
-    }); 
-});  
-
-router.delete('/boats/:boat_id', function(req, res) {
-    get_boat(req.params.boat_id)
-    .then (boat =>
-        {
-            if (boat[0] === undefined || boat[0] === null) 
-            {
-                // The 0th element is undefined. This means there is no lodging with this id
-                res.status(404).json({ 'Error': 'No boat with this boat_id exists' }).end(); 
-            }
-            else
-            {
-                delete_boat(req.params.boat_id).then(res.status(204).end()); 
-            }
-        })
-}); 
-
-router.put('/boats', function (req, res){
-    res.set('Accept', 'GET, POST');
-    res.status(405).end();
-});
-
-router.delete('/boats', function (req, res){
-    res.set('Accept', 'GET, POST');
-    res.status(405).end();
-});
-
-router.delete('/clear', function(req, res) {
-    get_boats().then  ( (boats) => {
-        for(var i = 0; i<boats.length; i++)
-        {
-            delete_boat(boats[i].id); 
-        }
-        res.status(204).end(); 
-    })
-})
 
 /* ------------- End Controller Functions ------------- */
 
-app.use('/', router);
+app.use('/lodgings', router);
+app.use('/login', login);
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });
