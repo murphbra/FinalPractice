f0e404b992f0d7b0aa21e882d08aa9e65938a2d6
murphbra <murphbra@oregonstate.edu>
2021-11-14 23:29:53 -0800

    update

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ router.post('/', checkJwt, function(req, res){
     }
 });
 */
-router.post('/', checkJwt, function(req, res, error, next){
+router.post('/', checkJwt, function(error, req, res, next){
     if (error.name === 'UnauthorizedError') {
         res.status(401).json({"Error": "JWT is invalid or missing"}).end(); 
     }
